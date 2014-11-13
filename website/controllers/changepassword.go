74d3c833b117b1b4c3f428702ef7da60b0bda5df
Aaron <confidence68@126.com>
2014-11-13 11:26:19 +0800

    修改密码绑定

--- website/controllers/changepassword.go
+++ website/controllers/changepassword.go
@@ -1,12 +1,34 @@
 package controllers
 
-import ()
+import (
+	"fmt"
+	"service/db"
+)
 
 type ChangePasswordController struct {
 	BaseController
 }
 
 func (this *ChangePasswordController) Post() {
+	mystruct := ""
+	if this.userinfo != nil {
+		this.Data["userinfo"] = this.userinfo
+		this.Data["doctotal"] = 0
+		var Lc_passwd = this.GetString("userpassword")
+		//var Old_passwd = this.GetString("oldpassword")
+		err := db.ExchangePwd(this.userinfo.Id, Lc_passwd)
+		if err != nil {
+			mystruct = `修改失败`
+		} else {
+			mystruct = `修改成功`
+		}
+		fmt.Println(err)
+	} else {
+		//用户未登录，返回修改失败
+		mystruct = `修改失败`
+	}
+	this.Data["err"] = mystruct
+	fmt.Println(mystruct)
 	this.TplNames = "pages/home/changePassword.html"
 }
 
